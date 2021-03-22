6396ab96fa9745a902509a1763cb9fd764056c86
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-22 15:42:05 +0100

    3 function on menu

--- whoyadulo.py
+++ whoyadulo.py
@@ -1,2 +1,5 @@
 print(" Witam jestem BOT MAN1EXTER " + "\U0001f600")
 print("Mam kilka zadań a oto kilka z nich:")
+print("[1] ZGADNE KIM JESTES")
+print("[2] POWIEM CI KTORA LICZBA JEST PARZYSTA, A KTORA NIE")
+print("[3] UTWORZE CI LOSOWY KLUCZ")
