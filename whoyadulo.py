6e7e6d550c9d3cf29af0b64e970413dccc9da8f0
Mariusz Perzynski <m.perzyk@interia.pl>
2021-03-23 10:37:37 +0100

    check posiiton nr.2

--- whoyadulo.py
+++ whoyadulo.py
@@ -12,11 +12,20 @@ if wybor == 1:
     imie = input("Jak masz na imie?")
     onano = print(imie.endswith("a"))
 
+    print(" ")
+    print("Witaj " + imie)
+
     if onano == False:
         print("Jestes kobieta")
     else:
         print("Jestes mezczyzna")
 
 elif wybor == 2:
-    
+
+    print("Pokaze ci parzyste i nieparzyste liczby w podanej liscie")
+    lista = [10,20,30,40,50,60,65,70,75,80,92,100]
+    print(lista)
+
+elif wybor == 3:
+
     print("ok")
